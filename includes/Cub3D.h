da87f27d350b2f76afaa76f15eb8eae2f915b4b9
ybounite <dimayoussef42@gmail.com>
2025-06-16 18:53:20 +0100

    add in include

---
+++ includes/Cub3D.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Cub3D.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ybounite <ybounite@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/16 14:57:09 by ybounite          #+#    #+#             */
+/*   Updated: 2025/06/16 15:37:36 by ybounite         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <math.h>
+
+# include "../libraries/getline/get_next_line.h"
+# include "../libraries/libft/libft.h"
+
+
+
+
+#endif
