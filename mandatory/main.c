cf13d222f1fd623f62e84e7ea4b6fd6d1f9f0f61
ybounite <dimayoussef42@gmail.com>
2025-06-20 18:59:51 +0100

    check file name

--- mandatory/main.c
+++ mandatory/main.c
@@ -6,17 +6,30 @@
 /*   By: ybounite <ybounite@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/16 15:45:16 by ybounite          #+#    #+#             */
-/*   Updated: 2025/06/16 18:51:17 by ybounite         ###   ########.fr       */
+/*   Updated: 2025/06/20 18:56:31 by ybounite         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/Cub3D.h"
 
-int main()
+void	parsing(char *filename)
 {
-	char *p = ft_malloc(5 , 1);
+	check_file_name(filename);
+	
+}
+void	syntax_error(short option)
+{
+	if (option == 1)
+		ft_putstr_fd("Error: \nInvalid arguments\n", 2);
+}
+int main(int ac, char **av)
+{
+	// char **map;
+	
+	if (ac != 2)
+		return (syntax_error(true), EXIT_FAILURE);
+	parsing(av[1]);
 
-	ft_strncpy(p, "hell", 4);
-	printf("%s\n", p);
+	printf("this name is valid !\n");
 	return 0;
 }
