efd283064290c5d9bfe9170343f12c04aabdef65
ybounite <dimayoussef42@gmail.com>
2025-06-28 11:11:11 +0100

    check in hindel file

--- mandatory/check_file_name.c
+++ mandatory/check_file_name.c
@@ -6,7 +6,7 @@
 /*   By: ybounite <ybounite@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/20 16:41:44 by ybounite          #+#    #+#             */
-/*   Updated: 2025/06/20 18:55:28 by ybounite         ###   ########.fr       */
+/*   Updated: 2025/06/26 13:19:52 by ybounite         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,9 +28,10 @@ bool	check_filename_cub(char *file_name, char *str)
 	}
 	return (true);
 }
-
+		
 void	check_file_name(char *file_name)
 {
+	// check if hindel file : 
 	if (!file_name || !check_filename_cub(file_name, ".cub"))
 	{
 		ft_putstr_fd("Erroe:\nfile name is incorrect!\n", 2);
