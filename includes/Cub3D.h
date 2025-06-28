4f7f6621bd6c8ae65fa40138f4412f6ebd4bb3c7
ybounite <dimayoussef42@gmail.com>
2025-06-28 10:53:09 +0100

    adding in prototype in functoin syntax_error

--- includes/Cub3D.h
+++ includes/Cub3D.h
@@ -6,7 +6,7 @@
 /*   By: ybounite <ybounite@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/16 14:57:09 by ybounite          #+#    #+#             */
-/*   Updated: 2025/06/20 19:22:33 by ybounite         ###   ########.fr       */
+/*   Updated: 2025/06/28 10:52:11 by ybounite         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,15 +22,27 @@
 # include "../libraries/getline/get_next_line.h"
 # include "../libraries/libft/libft.h"
 
-# define STDERR	2
-# define ALLOC	1
 # define CLEAR	0
+# define ALLOC	1
+# define STDERR	2
+
+typedef struct s_config
+{
+    char *no_texture;
+    char *so_texture;
+    char *we_texture;
+    char *ea_texture;
+    int floor_color;
+    int ceiling_color;
+} t_config;
 
 typedef struct	s_game
 {
 	short		fd;
 	char		**map;
 	char		**map_cpy;
+	t_list		*list;
+	t_config	config;
 }				t_data_game;
 
 /* -------------------------------------------------------------------------- */
@@ -38,5 +50,10 @@ typedef struct	s_game
 /* -------------------------------------------------------------------------- */
 bool			check_filename_cub(char *file_name, char *str);
 void			check_file_name(char *file_name);
+
+/* -------------------------------------------------------------------------- */
+/*                             syntax_error.c                                 */
+/* -------------------------------------------------------------------------- */
+void	        syntax_error(short option);
 
 #endif
