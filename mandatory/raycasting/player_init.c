4c8a55f1cd0337546a60c215e676944c7aff099a
ybounite <dimayoussef42@gmail.com>
2025-07-31 11:26:39 +0100

    convert postion player in maps to window

--- mandatory/raycasting/player_init.c
+++ mandatory/raycasting/player_init.c
@@ -6,7 +6,7 @@
 /*   By: ybounite <ybounite@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/08 14:41:31 by ybounite          #+#    #+#             */
-/*   Updated: 2025/07/21 11:46:55 by ybounite         ###   ########.fr       */
+/*   Updated: 2025/07/31 11:25:50 by ybounite         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -127,17 +127,17 @@ void	init_player_direction(t_data_game *_game, char spawn_dir)
 	if (spawn_dir == 'N') // North ^
 		_game->player->angle = PI / 2; // (1.57) 90°
 	else if (spawn_dir == 'S') // south 
-		_game->player->angle = PI * 1.5;
+		_game->player->angle = PI * 1.5; //  270°
 	else if (spawn_dir == 'E') // East
 		_game->player->angle = 0; // or 2PI (6.28) [0° / 360°]
 	else if (spawn_dir == 'W') // west
-		_game->player->angle = PI; // (3.14)
+		_game->player->angle = PI; // (3.14) 180°
 }
 /*   π/2 (1.57) [90°] [N]
                  ↑
-                 |
+				 |
 [W] π (3.14) ← player → 0 or 2π (6.28) [0° / 360°] [E]
-                 |
+      [180°]     |
                  ↓
     3π/2 (4.71) [270°] [S]
 */
@@ -145,4 +145,8 @@ void	player_init(t_data_game *_game)
 {
 	player_position(_game);
 	init_player_direction(_game, _game->spawm_dir);
+	// convert postion player maps to window *TITL_SIZE + (TITL_SIZE / 2)
+	_game->player->_x = (_game->player->_x * TILE_SIZE) + TILE_SIZE / 2;
+	_game->player->_y = (_game->player->_y * TILE_SIZE) + TILE_SIZE / 2;
+	// printf("postion palyer ot window --> x %f <-- and --> y %f <--\n", _game->player->_x, _game->player->_y);
 }
