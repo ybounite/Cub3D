9acbf19365dd7763a00b8fb1f636056a91d5aa4e
ybounite <dimayoussef42@gmail.com>
2025-08-11 16:37:38 +0100

    add management player module for handling player input and movement

---
+++ mandatory/manage_player/player_control.c
@@ -0,0 +1,67 @@
+
+#include "../../includes/Cub3D.h"
+
+
+
+void	_player_move(int key, t_data_game *_game)
+{
+	if (key == S_KEY)
+	{
+		_game->player->_x -= cos(_game->player->angle) * MOVE_SPEED;
+		_game->player->_y -= sin(_game->player->angle) * MOVE_SPEED;
+	}
+	else if (key == W_KEY)
+	{
+		_game->player->_x += cos(_game->player->angle) * MOVE_SPEED;
+		_game->player->_y += sin(_game->player->angle) * MOVE_SPEED;
+	}
+	else if (key == D_KEY)
+	{
+		_game->player->_x += cos(_game->player->angle - M_PI_2) * MOVE_SPEED;
+		_game->player->_y += sin(_game->player->angle - M_PI_2) * MOVE_SPEED;
+	}
+	else if (key == A_KEY)
+	{
+		_game->player->_x += cos(_game->player->angle + M_PI_2) * MOVE_SPEED;
+		_game->player->_y += sin(_game->player->angle + M_PI_2) * MOVE_SPEED;
+	}
+}
+
+int	control_key_(int keycode, t_data_game *_game)
+{
+	double	(old_player_x), (old_player_y);
+	(1) && (old_player_y = _game->player->_y), (old_player_x = _game->player->_x);
+	if (keycode == ESCAPE)
+	{
+		mlx_destroy_window(_game->_mlx, _game->_win_mlx);
+		ft_malloc(CLEAR, CLEAR);
+		exit(EXIT_SUCCESS);
+	}
+	_player_move(keycode, _game);
+	if (_hase_wall(_game, _game->player->_x, _game->player->_y))
+	{
+		_game->player->_x = old_player_x;
+	 	_game->player->_y = old_player_y;
+	}
+	if (keycode == LEFT_ARROW){
+		_game->player->angle -= ROTATION_SPEED;
+		printf("angle LEFT : %f\n", _game->player->angle);
+		if (_game->player->angle < 0)
+		{
+			_game->player->angle += 2 * PI;
+			printf("updat angle LEFT : %f\n", _game->player->angle);
+
+		}
+	}
+	else if (keycode == RIGHT_ARROW)
+	{
+		_game->player->angle += ROTATION_SPEED;
+		printf("angle RIGTH : %f\n", _game->player->angle);
+		if (_game->player->angle >= 2){
+			_game->player->angle -= 2 * PI; // keep angle
+			printf("updat angle RIGTH : %f\n", _game->player->angle);
+		}
+	}
+	return (true);
+}
+
---
+++ mandatory/manage_player/player_init.c
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   player_init.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ybounite <ybounite@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/08 14:41:31 by ybounite          #+#    #+#             */
+/*   Updated: 2025/07/31 11:25:50 by ybounite         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/Cub3D.h"
+
+bool	is_spawm_direction(char c)
+{
+	return (c == 'N' || c == 'S' || c == 'E' || c == 'W');
+}
+
+void	player_position(t_data_game *_game)
+{
+	short	x;
+	short	y;
+
+	y = 0;
+	while (_game->map[y])
+	{
+		x = 0;
+		while (_game->map[y][x])
+		{
+			if (is_spawm_direction(_game->map[y][x]))
+			{
+				_game->spawm_dir = _game->map[y][x];
+				_game->player->_x = x;
+				_game->player->_y = y;
+				break ;
+			}
+			x++;
+		}
+		y++;
+	}
+}
+
+void	init_player_direction(t_data_game *_game, char spawn_dir)
+{
+	if (spawn_dir == 'N') // North ^
+		_game->player->angle = (PI * 1.5); // (1.57) 90°
+	else if (spawn_dir == 'S') // south 
+		_game->player->angle = PI / 2; //  270°
+	else if (spawn_dir == 'E') // East
+		_game->player->angle = 0; // or 2PI (6.28) [0° / 360°]
+	else if (spawn_dir == 'W') // west
+		_game->player->angle = PI; // (3.14) 180°
+}
+/*   π/2 (1.57) [90°] [N]
+                 ↑
+				 |
+[W] π (3.14) ← player → 0 or 2π (6.28) [0° / 360°] [E]
+      [180°]     |
+                 ↓
+    3π/2 (4.71) [270°] [S]
+*/
+void	player_init(t_data_game *_game)
+{
+	player_position(_game);
+	init_player_direction(_game, _game->spawm_dir);
+	// convert postion player maps to window *TITL_SIZE + (TITL_SIZE / 2)
+	_game->player->_x = (_game->player->_x * TILE_SIZE) + TILE_SIZE / 2;
+	_game->player->_y = (_game->player->_y * TILE_SIZE) + TILE_SIZE / 2;
+	// printf("postion palyer ot window --> x %f <-- and --> y %f <--\n", _game->player->_x, _game->player->_y);
+}
