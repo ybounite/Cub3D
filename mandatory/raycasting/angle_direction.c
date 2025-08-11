58ea82a036f28649b3a599348b9dc1af871e549e
ybounite <dimayoussef42@gmail.com>
2025-08-11 13:41:35 +0100

    related to determining directions from angles

---
+++ mandatory/raycasting/angle_direction.c
@@ -0,0 +1,30 @@
+#include "../../includes/Cub3D.h"
+
+
+void    normalize_angle(double *angle)
+{
+	*angle = fmod(*angle, 2 * PI);
+	if (*angle < 0)
+		*angle += 2 * PI;
+}
+
+bool	is_facing_down(double angle)
+{
+	return (angle > 0 && angle < PI);
+}
+
+bool	is_facing_up(double angle)
+{
+	return !is_facing_down(angle);
+}
+
+bool	is_facing_right(double angle)
+{
+	return (angle < PI/2 || angle > 3*PI/2);
+}
+
+bool	is_facing_left(double angle)
+{
+	return !is_facing_right(angle);
+}
+
