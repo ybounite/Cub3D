b9f93f990e9066473716564387a058fc2d64fccd
ybounite <dimayoussef42@gmail.com>
2025-08-13 11:18:47 +0100

    implements vertical ray intersection detection to identify wall 🧱  collisions

---
+++ mandatory/raycasting/vertical_intersection.c
@@ -0,0 +1,43 @@
+
+
+#include "../../includes/Cub3D.h"
+
+bool	cast_vertical(t_data_game *g, t_ray *ray, t_point *hit, double *dist)
+{
+    t_point step;
+    t_point intercept;
+
+    intercept.x = floor(ray->player.x / TILE_SIZE) * TILE_SIZE;
+    if (is_facing_right(ray->ray_angle))
+        intercept.x += TILE_SIZE;
+
+    intercept.y = ray->player.y + (intercept.x - ray->player.x) * tan(ray->ray_angle);
+
+    step.x = TILE_SIZE * (is_facing_left(ray->ray_angle) ? -1 : 1);
+    step.y = TILE_SIZE * tan(ray->ray_angle);
+    if ((is_facing_up(ray->ray_angle) && step.y > 0) ||
+        (is_facing_down(ray->ray_angle) && step.y < 0))
+        step.y *= -1;
+
+    double next_x = intercept.x;
+    double next_y = intercept.y;
+
+    while (next_x >= 0 && next_y >= 0 &&
+           next_x < g->map_width * TILE_SIZE &&
+           next_y < g->map_height * TILE_SIZE)
+    {
+        int map_x = (int)((next_x + (is_facing_left(ray->ray_angle) ? -1 : 0)));
+        int map_y = (int)(next_y);
+
+        if (_hase_wall(g, map_x, map_y))
+	{
+            *hit = (t_point){next_x, next_y};
+            *dist = hypot(next_x - ray->player.x, next_y - ray->player.y);
+            return true;
+        }
+        next_x += step.x;
+        next_y += step.y;
+    }
+    return false;
+}
+
