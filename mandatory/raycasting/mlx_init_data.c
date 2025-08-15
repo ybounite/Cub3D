467d26b1772a31ba3f0a4ad19b3fa2dec922a3cc
ybounite <dimayoussef42@gmail.com>
2025-08-15 10:41:07 +0100

    Update _mlx_init_data to initialize window and image sizes to fixed WINDOW_WIDTH and WINDOW_HEIGHT constants

--- mandatory/raycasting/mlx_init_data.c
+++ mandatory/raycasting/mlx_init_data.c
@@ -6,7 +6,7 @@
 /*   By: ybounite <ybounite@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/14 15:45:32 by ybounite          #+#    #+#             */
-/*   Updated: 2025/07/21 09:51:52 by ybounite         ###   ########.fr       */
+/*   Updated: 2025/08/14 15:41:28 by ybounite         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,20 @@ bool	_mlx_init_data(t_data_game *_game)
 {
 	_game->_mlx = mlx_init();
 	// 														cols               rows
-	_game->_win_mlx = mlx_new_window(_game->_mlx, _game->map_width * TILE_SIZE, _game->map_height * TILE_SIZE, "Cub3D");
+	_game->_win_mlx = mlx_new_window(_game->_mlx, WINDOW_WIDTH, WINDOW_HEIGHT, "Cub3D");
 	if (!_game->_win_mlx)
 		return (ft_putendl_fd("Error:\nmlx_new_window failed.", STDERR), false);
 
-	_game->_img->img = mlx_new_image(_game->_mlx, _game->map_width * TILE_SIZE, _game->map_height * TILE_SIZE);
-	if (!_game->_img->img)	
+	_game->_img->img = mlx_new_image(_game->_mlx, WINDOW_WIDTH, WINDOW_HEIGHT);
+	if (!_game->_img->img)
 		return (ft_putendl_fd("Error:\nmlx_new_image failed.", STDERR), false);
 
 	printf("img ptr: %p\n", _game->_img->img);
 
-	_game->_img->addr = mlx_get_data_addr(_game->_img->img, &_game->_img->bits_per_pixel,
-							&_game->_img->line_length, &_game->_img->endian);
-	_game->_img->width = _game->map_width * TILE_SIZE;
-	_game->_img->height = _game->map_height * TILE_SIZE;
+	_game->_img->addr = mlx_get_data_addr(_game->_img->img, &_game->_img->bit_per_pixel,
+							&_game->_img->line_len, &_game->_img->endian);
+	_game->_img->width = WINDOW_WIDTH;
+	_game->_img->height = WINDOW_HEIGHT;
 	return true;
 }
+
