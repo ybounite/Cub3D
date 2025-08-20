5b77fca8362869872e6fa22d36351f0f43495f9b
ybounite <dimayoussef42@gmail.com>
2025-08-20 16:07:14 +0100

    initialize textures and their memory addresses for rendering

--- mandatory/render_texture/init_textures.c
+++ mandatory/render_texture/init_textures.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init_textures.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ybounite <ybounite@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/20 15:53:10 by ybounite          #+#    #+#             */
+/*   Updated: 2025/08/20 15:55:38 by ybounite         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../includes/Cub3D.h"
 
 void	init_textures(t_data_game *_game)
@@ -8,31 +20,57 @@ void	init_textures(t_data_game *_game)
 	_game->texture->SO = _game->config->textures[1];
 	_game->texture->WE = _game->config->textures[2];
 	_game->texture->EA = _game->config->textures[3];
+	printf("%s\n", _game->texture->EA);
+}
+
+void	init_textures_address(t_texture *texture, t_data_game *_game)
+{
+	(void)_game;
+	texture->NO_img->addr = mlx_get_data_addr(texture->NO_img->img,
+			&texture->NO_img->bit_per_pixel,
+			&texture->NO_img->line_len, &texture->NO_img->endian);
+	texture->EA_img->addr = mlx_get_data_addr(texture->NO_img->img,
+			&texture->EA_img->bit_per_pixel,
+			&texture->EA_img->line_len, &texture->EA_img->endian);
+	texture->WE_img->addr = mlx_get_data_addr(texture->NO_img->img,
+			&texture->WE_img->bit_per_pixel,
+			&texture->WE_img->line_len, &texture->WE_img->endian);
+	texture->SO_img->addr = mlx_get_data_addr(texture->NO_img->img,
+			&texture->SO_img->bit_per_pixel,
+			&texture->SO_img->line_len, &texture->SO_img->endian);
+	if (!texture->NO_img->addr || !texture->SO_img->addr
+		|| !texture->WE_img->addr || !texture->EA_img->addr)
+	{
+		ft_putendl_fd("Error: Address Failed to load textures.", STDERR);
+		ft_malloc(CLEAR, CLEAR);
+		exit(EXIT_FAILURE);
+	}
 }
 
 void	init_textures_data(t_data_game *_game)
 {
 	t_texture	*texture;
 
-	// Use the existing texture from _game
 	_game->texture->NO_img = ft_malloc(sizeof(t_imag), ALLOC);
 	_game->texture->SO_img = ft_malloc(sizeof(t_imag), ALLOC);
 	_game->texture->WE_img = ft_malloc(sizeof(t_imag), ALLOC);
 	_game->texture->EA_img = ft_malloc(sizeof(t_imag), ALLOC);
 	texture = _game->texture;
 	texture->NO_img->img = mlx_xpm_file_to_image(_game->_mlx, texture->NO,
-		&texture->NO_img->width, &texture->NO_img->height);
+			&texture->NO_img->width, &texture->NO_img->height);
 	texture->SO_img->img = mlx_xpm_file_to_image(_game->_mlx, texture->SO,
-		&texture->SO_img->width, &texture->SO_img->height);
+			&texture->SO_img->width, &texture->SO_img->height);
 	texture->WE_img->img = mlx_xpm_file_to_image(_game->_mlx, texture->WE,
-		&texture->WE_img->width, &texture->WE_img->height);
+			&texture->WE_img->width, &texture->WE_img->height);
 	texture->EA_img->img = mlx_xpm_file_to_image(_game->_mlx, texture->EA,
-		&texture->EA_img->width, &texture->EA_img->height);
-	if (!texture->NO_img->img || !texture->SO_img->img || !texture->WE_img->img
-		|| !texture->EA_img->img)
+			&texture->EA_img->width, &texture->EA_img->height);
+	if (!texture->NO_img->img || !texture->SO_img->img
+		|| !texture->WE_img->img || !texture->EA_img->img)
 	{
 		ft_putendl_fd("Error: Failed to load textures.", STDERR);
 		ft_malloc(CLEAR, CLEAR);
 		exit(EXIT_FAILURE);
 	}
+	init_textures_address(texture, _game);
+	_game->texture = texture;
 }
