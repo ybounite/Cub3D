a2ac6009df120e61b7110f63a40176f2f8d53c8f
Bouhcine Amezouar <bamezoua@c4r5p10.1337.ma>
2025-08-23 11:36:45 +0100

    fix Door parsing

--- mandatory/parsing/validate_map.c
+++ mandatory/parsing/validate_map.c
@@ -2,7 +2,8 @@
 
 bool	is_valid_cell(char c)
 {
-	return (c == '0' || c == 'N' || c == 'S' || c == 'E' || c == 'W' || c == 'D');
+	return (c == '0' || c == 'N' || c == 'S' || c == 'E' || c == 'W'
+		|| c == 'D');
 }
 
 bool	check_top_and_bottum(t_data_game *_game)
@@ -62,7 +63,6 @@ bool	check_map_if_closed(t_data_game *_game)
 	return (true);
 }
 
-
 //  if any 0 is closed by space or outside map => is not valide
 bool	is_valid_map(t_data_game *_game)
 {
@@ -79,6 +79,16 @@ bool	is_valid_map(t_data_game *_game)
 		width = ft_strlen(_game->map[i]);
 		while (j < width)
 		{
+			if (_game->map[i][j] == 'D')
+			{
+				if (!(_game->map[i][j - 1] == '1' && _game->map[i][j
+						+ 1] == '1') && !(_game->map[i - 1][j] == '1'
+						&& _game->map[i + 1][j] == '1'))
+				{
+					printf("Error\nDoor must be enclosed by walls on opposite sides\n");
+					return (false);
+				}
+			}
 			if (is_valid_cell(_game->map[i][j]))
 			{
 				if ((i == 0 || i == _game->map_height - 1) || (j == 0
