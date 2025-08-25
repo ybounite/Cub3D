7523fb04d21b53a5f88a611f8548e8457a546e3c
ybounite <dimayoussef42@gmail.com>
2025-08-25 14:16:27 +0100

    initialize game textures and door with error handling

--- mandatory/render_texture/init_textures.c
+++ mandatory/render_texture/init_textures.c
@@ -3,57 +3,44 @@
 /*                                                        :::      ::::::::   */
 /*   init_textures.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bamezoua <bamezoua@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ybounite <ybounite@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/08/20 15:53:10 by ybounite          #+#    #+#             */
-/*   Updated: 2025/08/24 11:10:22 by bamezoua         ###   ########.fr       */
+/*   Created: 2025/08/25 13:18:42 by ybounite          #+#    #+#             */
+/*   Updated: 2025/08/25 13:19:59 by ybounite         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #include "../../includes/Cub3D.h"
 
 void	init_textures(t_data_game *_game)
 {
 	_game->texture = ft_malloc(sizeof(t_texture), ALLOC);
-	_game->texture->NO = _game->config->textures[NO_TEXTURE];
-	_game->texture->SO = _game->config->textures[SO_TEXTURE];
-	_game->texture->WE = _game->config->textures[WE_TEXTURE];
-	_game->texture->EA = _game->config->textures[EA_TEXTURE];
+	_game->texture->no = _game->config->textures[NO_TEXTURE];
+	_game->texture->so = _game->config->textures[SO_TEXTURE];
+	_game->texture->we = _game->config->textures[WE_TEXTURE];
+	_game->texture->ea = _game->config->textures[EA_TEXTURE];
 	_game->texture->door = "./textures/open-door.xpm";
 }
 
-void	init_textures_address(t_texture *texture, t_data_game *_game)
+void	init_textures_address(t_texture *texture)
 {
-	(void)_game;
-	texture->NO_img->addr = mlx_get_data_addr(texture->NO_img->img,
-			&texture->NO_img->bit_per_pixel, &texture->NO_img->line_len,
-			&texture->NO_img->endian);
-	texture->EA_img->addr = mlx_get_data_addr(texture->EA_img->img,
-			&texture->EA_img->bit_per_pixel, &texture->EA_img->line_len,
-			&texture->EA_img->endian);
-	texture->WE_img->addr = mlx_get_data_addr(texture->WE_img->img,
-			&texture->WE_img->bit_per_pixel, &texture->WE_img->line_len,
-			&texture->WE_img->endian);
-	texture->SO_img->addr = mlx_get_data_addr(texture->SO_img->img,
-<<<<<<< HEAD
-			&texture->SO_img->bit_per_pixel,
-			&texture->SO_img->line_len, &texture->SO_img->endian);
+	texture->no_img->addr = mlx_get_data_addr(texture->no_img->img,
+			&texture->no_img->bit_per_pixel, &texture->no_img->line_len,
+			&texture->no_img->endian);
+	texture->ea_img->addr = mlx_get_data_addr(texture->ea_img->img,
+			&texture->ea_img->bit_per_pixel, &texture->ea_img->line_len,
+			&texture->ea_img->endian);
+	texture->we_img->addr = mlx_get_data_addr(texture->we_img->img,
+			&texture->we_img->bit_per_pixel, &texture->we_img->line_len,
+			&texture->we_img->endian);
+	texture->so_img->addr = mlx_get_data_addr(texture->so_img->img,
+			&texture->so_img->bit_per_pixel,
+			&texture->so_img->line_len, &texture->so_img->endian);
 	texture->dr_img->addr = mlx_get_data_addr(texture->dr_img->img,
 			&texture->dr_img->bit_per_pixel,
 			&texture->dr_img->line_len, &texture->dr_img->endian);
 
-	if (!texture->NO_img->addr || !texture->SO_img->addr || !texture->dr_img->addr
-		|| !texture->WE_img->addr || !texture->EA_img->addr)
-=======
-			&texture->SO_img->bit_per_pixel, &texture->SO_img->line_len,
-			&texture->SO_img->endian);
-	texture->Door_img->addr = mlx_get_data_addr(texture->Door_img->img,
-			&texture->Door_img->bit_per_pixel, &texture->Door_img->line_len,
-			&texture->Door_img->endian);
-	if (!texture->NO_img->addr || !texture->SO_img->addr
-		|| !texture->WE_img->addr || !texture->EA_img->addr
-		|| !texture->Door_img->addr)
->>>>>>> 843938568d64852f4f288043d708b420f1babcc0
+	if (!texture->no_img->addr || !texture->so_img->addr || !texture->dr_img->addr
+		|| !texture->we_img->addr || !texture->ea_img->addr)
 	{
 		ft_putendl_fd("Error: Address Failed to load textures.", STDERR);
 		ft_malloc(CLEAR, CLEAR);
@@ -65,40 +52,29 @@ void	init_textures_data(t_data_game *_game)
 {
 	t_texture	*texture;
 
-	_game->texture->NO_img = ft_malloc(sizeof(t_imag), ALLOC);
-	_game->texture->SO_img = ft_malloc(sizeof(t_imag), ALLOC);
-	_game->texture->WE_img = ft_malloc(sizeof(t_imag), ALLOC);
-	_game->texture->EA_img = ft_malloc(sizeof(t_imag), ALLOC);
-<<<<<<< HEAD
+	_game->texture->no_img = ft_malloc(sizeof(t_imag), ALLOC);
+	_game->texture->so_img = ft_malloc(sizeof(t_imag), ALLOC);
+	_game->texture->we_img = ft_malloc(sizeof(t_imag), ALLOC);
+	_game->texture->ea_img = ft_malloc(sizeof(t_imag), ALLOC);
 	_game->texture->dr_img = ft_malloc(sizeof(t_imag), ALLOC);
-=======
-	_game->texture->Door_img = ft_malloc(sizeof(t_imag), ALLOC);
->>>>>>> 843938568d64852f4f288043d708b420f1babcc0
 	texture = _game->texture;
-	texture->NO_img->img = mlx_xpm_file_to_image(_game->_mlx, texture->NO,
-			&texture->NO_img->width, &texture->NO_img->height);
-	texture->SO_img->img = mlx_xpm_file_to_image(_game->_mlx, texture->SO,
-			&texture->SO_img->width, &texture->SO_img->height);
-	texture->WE_img->img = mlx_xpm_file_to_image(_game->_mlx, texture->WE,
-			&texture->WE_img->width, &texture->WE_img->height);
-	texture->EA_img->img = mlx_xpm_file_to_image(_game->_mlx, texture->EA,
-			&texture->EA_img->width, &texture->EA_img->height);
-<<<<<<< HEAD
+	texture->no_img->img = mlx_xpm_file_to_image(_game->_mlx, texture->no,
+			&texture->no_img->width, &texture->no_img->height);
+	texture->so_img->img = mlx_xpm_file_to_image(_game->_mlx, texture->so,
+			&texture->so_img->width, &texture->so_img->height);
+	texture->we_img->img = mlx_xpm_file_to_image(_game->_mlx, texture->we,
+			&texture->we_img->width, &texture->we_img->height);
+	texture->ea_img->img = mlx_xpm_file_to_image(_game->_mlx, texture->ea,
+			&texture->ea_img->width, &texture->ea_img->height);
 	texture->dr_img->img = mlx_xpm_file_to_image(_game->_mlx, "textures/door2.xpm",
 			&texture->dr_img->width, &texture->dr_img->height);
-	if (!texture->NO_img->img || !texture->SO_img->img || !texture->dr_img->img
-		|| !texture->WE_img->img || !texture->EA_img->img)
-=======
-	texture->Door_img->img = mlx_xpm_file_to_image(_game->_mlx, texture->Door,
-			&texture->Door_img->width, &texture->Door_img->height);
-	if (!texture->NO_img->img || !texture->SO_img->img || !texture->WE_img->img
-		|| !texture->EA_img->img || !texture->Door_img->img)
->>>>>>> 843938568d64852f4f288043d708b420f1babcc0
+	if (!texture->no_img->img || !texture->so_img->img || !texture->dr_img->img
+		|| !texture->we_img->img || !texture->ea_img->img)
 	{
 		ft_putendl_fd("Error: Failed to load textures.", STDERR);
 		ft_malloc(CLEAR, CLEAR);
 		exit(EXIT_FAILURE);
 	}
-	init_textures_address(texture, _game);
+	init_textures_address(texture);
 	_game->texture = texture;
 }
